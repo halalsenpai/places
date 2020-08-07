ab98b969beb97b219b923c795a76ddf155cd0676
Abdullah Chaghtai <55468900+halalsenpai@users.noreply.github.com>
2020-08-07 16:53:55 +0500

    upp

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(24.898663,67.1154261),
+  zoom:4,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
